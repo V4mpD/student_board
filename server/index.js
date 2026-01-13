fd367945994218911478995a52ff1d733a840f7b
V4mpD <daniel.baciu.or@gmail.com>
2026-01-13 17:38:35 +0200

    render fix 1

--- server/index.js
+++ server/index.js
@@ -275,9 +275,9 @@ io.on('connection', (socket) => {
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../build')));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../build', 'index.html'));
-    });
+    app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 }
 
 server.listen(PORT, () => {
